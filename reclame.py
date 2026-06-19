9978ea00c057c306458a14d684b3589618ef7ff6
Hermie Tineke <tineke.hermie@azdelta.be>
2026-06-19 16:45:51 +0200

    functies toegevoegd

---
+++ reclame.py
@@ -0,0 +1,33 @@
+from algemene_functies import mijn_functie_2
+
+def aanbieding_1(smaak, prijs, korting):
+    nieuwe_prijs=prijs*(1-korting)
+    return f"Vandaag in de aanbieding: emmertje ijs (1 liter) in de smaak {smaak}, van {prijs} euro voor {nieuwe_prijs:.2f} euro."
+
+def inkomsten_totaal(inkomsten):
+    totaal=0
+    for bedrag in inkomsten: totaal+=bedrag
+    return totaal
+
+def inkomsten_totaal(inkomsten,btw):
+    totaal=0
+    for bedrag in inkomsten: totaal+=bedrag
+    bedrag=totaal*btw
+    return f"Het totaal van alle inkomsten van deze week is {totaal} euro, waarvoor {bedrag} tbw betaald dient te worden."
+
+def laag_en_hoog(mijn_lijst):
+    return [max(mijn_lijst), min(mijn_lijst)]
+
+def gemiddelde(mijn_lijst):
+    return sum(mijn_lijst)/len(mijn_lijst)
+
+def gemiddelde(mijn_lijst):
+    bedrag=sum(mijn_lijst)/len(mijn_lijst)
+    return f"De gemiddelde inkomsten deze week zijn {bedrag} euro."
+
+def meervoudig(invoer_lijst):
+    return laag_en_hoog(invoer_lijst)
+
+def combinatie(invoer_lijst_2):
+    korte_lijst=laag_en_hoog(invoer_lijst_2)
+    return mijn_functie_2(korte_lijst)
